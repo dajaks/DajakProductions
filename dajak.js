46549f6e37a637a9a8a9fd6ed1852c46e631afe9
dajaks <codyjakubowski9911@gmail.com>
2025-09-21 17:38:12 -0600

    added media queiries

--- dajak.js
+++ dajak.js
@@ -4,8 +4,12 @@ const homeButton = document.getElementById('home-button');
 const projectButton = document.getElementById('project-button');
 const partnerButton = document.getElementById('partner-button');
 const socialButton = document.getElementById('social-button');
+const closeButton = document.querySelector('.close-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+const navIcon = document.getElementById('nav-icon');
 //Below essentially changes .nav-btn to an array so we can use methods
 const navButtons = document.querySelectorAll('.nav-btn');
+const socialButtons = document.querySelectorAll('.social-logo');
 //button change-function
 
 const buttonColor = (button) => {
@@ -18,6 +22,7 @@ const returnButtonColor = (button) => {
     button.target.style.color = ''
 }
 
+
 //application of button change
 
 navButtons.forEach(btn => {
@@ -25,3 +30,28 @@ navButtons.forEach(btn => {
     btn.onmouseleave = returnButtonColor;
 }
 )
+
+//social button color change
+
+socialButtons.forEach(btn => {
+    btn.onmouseover = buttonColor;
+    btn.onmouseleave = returnButtonColor;
+}
+)
+
+//closing the nav bar for mobile
+
+const openNav = () => {
+   mobileNav.classList.add('open');
+   navIcon.classList.add('hidden');
+};
+
+const exitNav = () => {
+    mobileNav.classList.remove('open');
+    navIcon.classList.remove('hidden')
+};
+
+closeButton.onclick = exitNav;
+navIcon.onclick = openNav;
+
+
